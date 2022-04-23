281eb265089ff7c18b8bcfc55f647c3543adf600
Kristen <iamkristen220@gmail.com>
2022-04-23 18:25:44 +0545

    role done

--- lib/view model/database_manager.dart
+++ lib/view model/database_manager.dart
@@ -3,17 +3,16 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class DataBaseManager {
-  static DatabaseReference ref =
-      FirebaseDatabase.instance.ref().child('parents');
+  static DatabaseReference ref = FirebaseDatabase.instance.ref().child('users');
   Future<void> createParent(
-      String uid, String name, String email, String role) async {
+      String uid, String name, String email, bool isAdmin) async {
     try {
       int dt = DateTime.now().millisecondsSinceEpoch;
       ref.child(uid).set({
         'uid': uid,
         'name': name,
         'email': email,
-        'role': role,
+        'isAdmin': isAdmin,
         'dt': dt,
       });
     } on FirebaseAuthException catch (e) {
